ac0f2cfc2407f98d7e66147eff0b68bc0328a2af
opizarro98 <131551382+opizarro98@users.noreply.github.com>
2024-01-08 21:10:21 -0500

    OP - add contactos

--- KALAgym/src/app/app.component.ts
+++ KALAgym/src/app/app.component.ts
@@ -3,14 +3,22 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CarrucelComponent } from './carrucel/carrucel.component';
 import { ServiciosComponent } from './servicios/servicios.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,CarrucelComponent, ServiciosComponent],
+  imports: [CommonModule, RouterOutlet,CarrucelComponent, ServiciosComponent, ContactoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'KALAgym';
+
+   txtmenu1: string = 'Inicio';
+   txtmenu2: string = 'Servicios';
+   txtmenu3: string = 'Productos';
+   txtmenu4: string = 'Contacto';
 }
+
+
